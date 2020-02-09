6af16da76a91cb739b311ff4c8b8207f5ce2adb5
Philip B <me@crabman.me>
2020-02-09 20:52:22 +0300

    Add unittests for ConvSBS.{mean,std,fro_norm} and khrulkov init

--- tests/test_conv_sbs.py
+++ tests/test_conv_sbs.py
@@ -3,18 +3,20 @@ import pytest
 import torch
 
 from dctn.conv_sbs_spec import SBSSpecCore, SBSSpecString, Pos2D
-from dctn.conv_sbs import ConvSBS, KhrulkovNormalInitialization
+from dctn.conv_sbs import ConvSBS
 
 
 @pytest.mark.parametrize(
     "num_monte_carlo_iterations, bond_size, in_num_channels, in_quantum_dim_size, trace_edge, desired_std, allowed_relative_error",
     (
-        (40, 5, 2, 2, False, 0.5, 0.3),
-        (40, 1, 3, 3, True, 100.0, 0.3),
-        (40, 20, 1, 4, False, 7.0, 0.3),
+        (1000, 5, 2, 2, False, 0.5, 0.3),
+        (1500, 6, 2, 2, False, 100.0, 0.5),
+        (1000, 12, 1, 4, False, 7.0, 0.5),
+        (2000, 4, 2, 2, False, 1.0, 0.5),
+        (1000, 8, 1, 2, True, 0.1, 0.5),
     ),
 )
-def test_one_combination(
+def test_khrulkov_normal_init_and_std(
     num_monte_carlo_iterations,
     bond_size,
     in_num_channels,
@@ -44,9 +46,16 @@ def test_one_combination(
 
         conv_sbs = ConvSBS(spec, KhrulkovNormalInitialization(desired_std))
         std_fast = conv_sbs.var() ** 0.5
-        std_slow = conv_sbs.as_explicit_tensor().std()
-        assert torch.allclose(std_fast, std_slow)
         empiric_stds.append(std_fast.item())
+        if i == 0:
+            explicit_tensor = conv_sbs.as_explicit_tensor()
+            std_slow = explicit_tensor.std()
+            rtol = 1e-3
+            assert torch.allclose(std_slow, std_fast, rtol=rtol)
+            assert torch.allclose(explicit_tensor.mean(), conv_sbs.mean(), rtol=rtol)
+            assert torch.allclose(
+                explicit_tensor.norm("fro"), conv_sbs.fro_norm(), rtol=rtol
+            )
 
     mean_empiric_std = sum(empiric_stds) / len(empiric_stds)
     relative_error = abs(desired_std - mean_empiric_std) / desired_std
