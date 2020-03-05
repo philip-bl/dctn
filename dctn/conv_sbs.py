Fix mistake with the convsbs scaling code

dctn/conv_sbs.py
@@ -333,15 +333,12 @@ class ConvSBS(nn.Module):
         ]
         return result
 
-    def __imul__(self, scalar: float):
-        """ConvSBS is a tensor network. This method performs *= operator by multiplying this tensor network
-        by scalar."""
+    def multiply_by_scalar(self, scalar: float):
+        """ConvSBS is a tensor network. This method multiplies this tensor network
+        by scalar inplace."""
         for core in self.cores:
             core *= scalar ** (1 / len(self.cores))
         return self
-
-    def __idiv__(self, scalar: float):
-        return self.__imul__(scalar ** -1)
 
 
 class ManyConvSBS(nn.Module):

mnist.py
@@ -272,7 +272,7 @@ class DCTNMnistModel(nn.Module):
                     conv_sbs.strings, intermediate_before_rescaling
                 ):
                     std = tensor.std().item()
-                    string /= std
+                    string.multiply_by_scalar(std ** -1)
                     logger.info(f"Divided a ConvSBS by {std}")
                 intermediate_after_rescaling = conv_sbs(intermediate_after_rescaling)
                 for tensor in intermediate_after_rescaling:
@@ -339,7 +339,9 @@ def add_quantum_inputs_statistics_logging(
     type=float,
     help="For dumb-normal this sets std of each core of each sbs core; for khrulkov-normal this sets std of each sbs whole tensor",
 )
-@click.option("--scale-layers-using-batch", type=int, help="Pass batch size for scaling here.")
+@click.option(
+    "--scale-layers-using-batch", type=int, help="Pass batch size for scaling here."
+)
 @click.option("--epochs", type=int, default=5000)
 @click.option("--early-stopping-patience-num-epochs", type=int)
 @click.option("--warmup-num-epochs", "-w", type=int, default=40)
@@ -444,7 +446,11 @@ def main(
     elif scale_layers_using_batch is not None:
         model.scale_layers_using_batch(
             next(
-                iter(DataLoader(dataset, batch_size=scale_layers_using_batch, shuffle=True))
+                iter(
+                    DataLoader(
+                        dataset, batch_size=scale_layers_using_batch, shuffle=True
+                    )
+                )
             )[0]
         )
         logger.info("Done model.scale_layers_using_batch")