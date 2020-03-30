3d6c79c975643eec680275a8eb8d816e9328b9d7
Philip B <me@crabman.me>
2020-03-30 22:15:27 +0300

    Sligthyl change dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
-
 ((python-mode
+  (python-shell-interpreter . "ipython")
   (python-shell-interpreter-args . "-i --simple-prompt -c \"%autoreload 2\"")))
