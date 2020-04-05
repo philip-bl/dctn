6303b07c2e65c8206c6eaf7f4840d96a4f8b02d3
Philip B <me@crabman.me>
2020-04-05 16:19:01 +0300

    Replace testing as_explicit_tensor with testing as_eps

--- tests/test_conversion_of_convsbs_to_eps.py
+++ tests/test_conversion_of_convsbs_to_eps.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 import torch
 
 from einops import rearrange
@@ -8,39 +10,36 @@ from dctn.conv_sbs import ConvSBS
 from dctn.eps import eps
 
 
-def test_simple_conversion() -> None:
-  """Test conversion when the ConvSBS has kernel_size*kernel_size cores in the most
-  straightforward order."""
+def test_conversion() -> None:
   in_num_channels = 2
   in_quantum_dim_size = 2
-  batch_size = 6
-  height = 8
-  width = 9
-  spec = SBSSpecString(
-    (SBSSpecCore(Pos2D(0, 0), 1), SBSSpecCore(Pos2D(0, 1), 1),
-     SBSSpecCore(Pos2D(1, 0), 4), SBSSpecCore(Pos2D(1, 1), 1)),
-    (5, 5, 5, 5), in_num_channels, in_quantum_dim_size)
-  convsbs = ConvSBS(spec).double()
-  eps_tensor = rearrange(
-    convsbs.as_explicit_tensor().detach(), "... () () outquantum () -> ... outquantum")
-  assert eps_tensor.shape == (2,2, 2,2, 2,2, 2,2, 4)
+  batch_size = 3
+  height = 4
+  width = 5
+  cores = (SBSSpecCore(Pos2D(0, 0), 1), SBSSpecCore(Pos2D(0, 1), 3),
+                 SBSSpecCore(Pos2D(1, 0), 2), SBSSpecCore(Pos2D(1, 1), 4))
+  for cores_permutation in permutations(cores):
+    spec = SBSSpecString(cores_permutation, (3, 4, 5, 6), in_num_channels, in_quantum_dim_size)
+    convsbs = ConvSBS(spec).double()
+    with torch.no_grad():
+      eps_tensor = convsbs.as_eps()
+    assert eps_tensor.shape == (2,2, 2,2, 2,2, 2,2, 1*2*3*4)
+    assert torch.all(eps_tensor == convsbs.as_eps())
 
-  # input = torch.randn(
-  #   in_num_channels, batch_size, height, width, in_quantum_dim_size,
-  #   dtype=torch.float64, requires_grad=True)
-  input = torch.ones(
-    in_num_channels, batch_size, height, width, in_quantum_dim_size,
-    dtype=torch.float64, requires_grad=True)
+    input = torch.randn(
+      in_num_channels, batch_size, height, width, in_quantum_dim_size,
+      dtype=torch.float64, requires_grad=True)
 
-  convsbs_output = convsbs(input)
-  out_grad = torch.randn_like(convsbs_output)
-  convsbs_output.backward(out_grad)
-  convsbs_input_grad = input.grad.clone()
-  input.grad.zero_()
+    convsbs_output = convsbs(input)
+    out_grad = torch.randn_like(convsbs_output)
+    convsbs_output.backward(out_grad)
+    convsbs_input_grad = input.grad.clone()
+    input.grad.zero_()
 
-  eps_output = eps(eps_tensor, input)
-  assert torch.allclose(eps_output, convsbs_output)
-  eps_output.backward(out_grad)
-  eps_input_grad = input.grad.clone()
-  input.grad.zero_()
-  assert torch.allclose(eps_input_grad, convsbs_input_grad)
+    eps_output = eps(eps_tensor, input)
+    assert torch.allclose(eps_output, convsbs_output)
+    eps_output.backward(out_grad)
+    eps_input_grad = input.grad.clone()
+    input.grad.zero_()
+    assert torch.allclose(eps_input_grad, convsbs_input_grad)
+
