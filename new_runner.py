57142d58e4e48bb812f3cb5b2eef3ef4e0bc74bc
Philip B <me@crabman.me>
2020-04-14 23:28:28 +0300

    Add stuff in new_runner.py

--- new_runner.py
+++ new_runner.py
@@ -7,6 +7,8 @@ import logging
 import click
 
 import torch
+from torch.optim import Adam, SGD
+import torch.nn.functional as F
 
 from libcrap import get_now_as_str, save_json
 from libcrap.torch import set_random_seeds
@@ -14,6 +16,8 @@ from libcrap.torch import set_random_seeds
 from dctn.eps_plus_linear import EPSPlusLinear
 from dctn.evaluation import score
 from dctn.dataset_loading import get_fashionmnist_data_loaders, get_mnist_data_loaders
+from dctn.training import (
+  train, every_n_iters_intervals, LastModelsCheckpointer, BestModelCheckpointer, ValuesNotImprovingEarlyStopper)
 
 DIFF_FNAME = "git_diff_with_HEAD.patch"
 RUN_INFO_FNAME = "run_info.txt"
@@ -43,6 +47,10 @@ def get_git_commit_info() -> str:
 @click.option("--out-size", type=int, required=True)
 @click.option("--batch-size", type=int, required=True)
 @click.option("--load-model-state", type=click.Path(exists=True, dir_okay=False))
+@click.option("--optimizer", type=click.Choice(("adam", "sgd"), case_sensitive=False))
+@click.option("--lr", type=float)
+@click.option("--wd", type=float, help="weight decay", default=0.)
+@click.option("--patience", type=int, help="early stopping patience num evaluations", default=20)
 def main(**kwargs) -> None:
   kwargs["output_dir"] = join(kwargs["experiments_dir"], get_now_as_str(False, True, True))
   assert not os.path.exists(kwargs["output_dir"])
@@ -56,7 +64,7 @@ def main(**kwargs) -> None:
     level=kwargs["verbosity"], handlers=(
       logging.StreamHandler(),
       logging.FileHandler(join(kwargs["output_dir"], LOG_FNAME), "w", "utf-8")),
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", force=True)
   logger = logging.getLogger(__name__)
   logger.info(f"{kwargs['output_dir']=}")
 
@@ -69,11 +77,28 @@ def main(**kwargs) -> None:
   train_dl, val_dl, test_dl = get_dls(
     kwargs["ds_path"], kwargs["batch_size"], dev, autoscale_kernel_size=kwargs["kernel_size"])
   set_random_seeds(dev, kwargs["seed"])
-  logger.info("On train loss={0:.4f}, acc={1:.2%}".format(*score(model, train_dl, dev)))
-  logger.info("On val   loss={0:.4f}, acc={1:.2%}".format(*score(model, val_dl, dev)))
-  logger.info("On test  loss={0:.4f}, acc={1:.2%}".format(*score(model, test_dl, dev)))
-  breakpoint()
 
+  eval_schedule = every_n_iters_intervals((10, 1), (100, 10), (1000, 100), (20000, 1000), (None, 10000))
+
+  @eval_schedule
+  def evaluate_and_log(st_x, st_it):
+    st_x["model"].eval()
+    st_it["train_mean_ce"], st_it["train_acc"] = score(st_x["model"], train_dl, st_x["dev"])
+    st_it["val_mean_ce"], st_it["val_acc"] = score(st_x["model"], val_dl, st_x["dev"])
+    logger.info(f"After {st_it['num_iters_done']:07} iters: "
+                f"train/val mean_ce={st_it['train_mean_ce']:.5f}/{st_it['val_mean_ce']:.5f} "
+                f"acc={st_it['train_acc']:.2%}/{st_it['val_acc']:.2%}")
+
+  last_models_checkpointer = eval_schedule(LastModelsCheckpointer(kwargs["output_dir"], 10))
+  metrics = (("train_acc", False), ("val_acc", False), ("train_mean_ce", True), ("val_mean_ce", True))
+  best_value_checkpointers = tuple(
+    eval_schedule(BestModelCheckpointer(kwargs["output_dir"], *metric)) for metric in metrics)
+  early_stopper = eval_schedule(ValuesNotImprovingEarlyStopper(kwargs["patience"], metrics))
+  optimizer = {"adam": Adam, "sgd": SGD}[kwargs["optimizer"]](
+    model.parameters(), kwargs["lr"], weight_decay=kwargs["wd"])
+  st_x, st_it = train(
+    train_dl, model, optimizer, kwargs["device"], F.cross_entropy,
+    [evaluate_and_log, last_models_checkpointer, *best_value_checkpointers, early_stopper], [], [])
 
 
 if __name__ == "__main__":
--- small_experiments/blah_new_runner.py
+++ small_experiments/blah_new_runner.py
@@ -6,29 +6,9 @@ main.main(
     "--ds-type", "fashionmnist",
     "--ds-path", "/mnt/hdd_1tb/datasets/fashionmnist",
     "--kernel-size", "4",
-    "--out-size", "4",
+    "--out-size", "6",
     "--batch-size", "128",
-    # "--load-model-state", "/mnt/important/experiments/eps_plus_linear_fashionmnist/out_size=4_model/model_sd.pth"
+    "--optimizer", "adam",
+    "--lr", "1e-3"
   ),
   standalone_mode=False)
-
-
-
-# STOP HERE, run interact, do the lines below
-from dctn.align import align
-from dctn.rank_one_tensor import RankOneTensorsBatch
-train_ds = train_dl.dataset
-x = train_dl.dataset.x.double()
-x_windows = torch.cat(
-  tuple(
-    torch.stack(tuple(align(x_slice, kwargs["kernel_size"])), dim=0)
-    for x_slice in x[:, 22000:44000].split(kwargs["batch_size"], dim=1)),
-  dim=1)
-x_windows_r1t = RankOneTensorsBatch(x_windows, factors_dim=0, coordinates_dim=4)
-assert x_windows_r1t.ncoordinates == 2**16
-print(f"{x_windows_r1t.mean_over_batch()=}")
-print(f"{x_windows_r1t.std_over_batch()=}")
-from dctn.utils import transform_dataset
-x_transformed = transform_dataset(model[0].double(), x.to(dev))
-print(f"{x_transformed.mean()=}")
-print(f"{x_transformed.std()=}")
