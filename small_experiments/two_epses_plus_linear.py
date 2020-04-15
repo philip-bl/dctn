fc8308378d1a5308cc5fd6e44aff05c1e67d2268
Philip B <me@crabman.me>
2020-04-15 19:21:30 +0300

    Add two_epses_plus_linear.py

---
+++ small_experiments/two_epses_plus_linear.py
@@ -0,0 +1,13 @@
+from new_runner import main
+
+main.main(
+  (
+    "--experiments-dir", "/tmp/new_runner_test/",
+    "--ds-type", "fashionmnist",
+    "--ds-path", "/mnt/hdd_1tb/datasets/fashionmnist",
+    "--epses-specs", "(4,4)",
+    "--batch-size", "128",
+    "--optimizer", "adam",
+    "--lr", "1e-3"
+  ),
+  standalone_mode=False)
