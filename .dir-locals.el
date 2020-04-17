f8e8f04cd2ae48ff156bfb5963a5b18e3e850da8
Philip B <me@crabman.me>
2020-04-17 20:18:20 +0300

    Add local settings for black code formatting

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 ((python-mode
   (python-shell-interpreter . "ipython")
-  (python-shell-interpreter-args . "-i --simple-prompt -c \"%autoreload 2\"")))
+  (python-shell-interpreter-args . "-i --simple-prompt -c \"%autoreload 2\"")
+  (eval add-hook 'before-save-hook 'elpy-format-code nil t)))
