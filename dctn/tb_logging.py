1e245f0d57959a1f4e9f609b6584e752258f61ac
Philip B <me@crabman.me>
2020-04-26 14:29:41 +0300

    improve add_good_bad_bar

--- dctn/tb_logging.py
+++ dctn/tb_logging.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn.functional as F
 
@@ -18,27 +20,35 @@ def add_good_bad_bar(img: torch.Tensor, how_good: float) -> torch.Tensor:
     padded = F.pad(img.expand(3, *img.shape[1:]), (padding,) * 4, value=1.0)
     new_width = padded.shape[2]
 
-    bar_width = round(how_good * new_width)
-    if how_good >= 0.5:
-        channel = 1
-        value = (how_good - 0.5) * 2
+    if not math.isfinite(how_good):
+        # Pink is #FF54FF
+        padded[0, -padding:] = 0xFF / 255.0
+        padded[1, -padding:] = 0x54 / 255.0
+        padded[2, -padding:] = 0xFF / 255.0
     else:
-        channel = 0
-        value = (0.5 - how_good) * 2
-    for c in range(3):
-        if c == channel:
-            padded[c, -padding:, :bar_width] = value
+        bar_width = round(how_good * new_width)
+        if how_good >= 0.5:
+            channel = 1
+            value = (how_good - 0.5) * 2
+            bar_width = round((how_good - 0.5) * 2 * new_width)
         else:
-            padded[c, -padding:, :bar_width] = 0.0
+            channel = 0
+            value = (0.5 - how_good) * 2
+            bar_width = round((0.5 - how_good) * 2 * new_width)
+        for c in range(3):
+            if c == channel:
+                padded[c, -padding:, :bar_width] = value
+            else:
+                padded[c, -padding:, :bar_width] = 0.0
     return padded
 
 
-def add_index_dots(img: torch.Tensor, index: int, padding: int = 3) -> torch.Tensor:
+def add_y_dots(img: torch.Tensor, y: int, padding: int = 3) -> torch.Tensor:
     assert img.ndim == 3
     assert img.shape[0] == 3
-    assert img.shape[2] >= index * 2
+    assert img.shape[2] >= y * 2
     new_img = img.clone()
-    for i in range(index):
+    for i in range(y):
         new_img[2, :padding, 2 * i] = 1.0
         new_img[:2, :padding, 2 * i] = 0.0
     return new_img
