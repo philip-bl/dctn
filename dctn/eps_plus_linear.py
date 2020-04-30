85af76102f44af73deb4fb5d2f451f9741bd6f0b
Philip B <me@crabman.me>
2020-05-01 00:18:18 +0300

    Started but didnt finish rewriting EPSesPlusLinear

--- dctn/eps_plus_linear.py
+++ dctn/eps_plus_linear.py
@@ -1,16 +1,75 @@
 from functools import reduce
 import operator
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Union
 
 from more_itertools import intersperse
+from attr import attrs, attrib
 
 import torch
+from torch import Tensor
 import torch.nn as nn
 
 from einops.layers.torch import Rearrange
 
+from . import eps
 from dctn.eps import EPS
 from . import epses_composition
+
+
+@attrs(auto_attribs=True, frozen=True)
+class UnitEmpiricalOutputStd:
+    input: Tensor
+    batch_size = attrib(default=128)
+
+
+class UnitTheoreticalOutputStd:
+    pass
+
+
+Initialization = Union[UnitEmpiricalOutputStd, UnitTheoreticalOutputStd]
+
+
+class EPSesPlusLinear(nn.Module):
+    def __init__(
+        self,
+        epses_specs: Tuple[Tuple[int, int]],
+        initialization: Initialization,
+        device: torch.device,
+        dtype: torch.dtype,
+    ):
+        super().__init__()
+        if isinstance(initialization, UnitEmpiricalOutputStd):
+            self.epses = nn.ParameterList(
+                epses_composition.make_epses_composition_unit_empirical_output_std(
+                    epses_specs, initialization.input, device, dtype, initialization.batch_size
+                )
+            )
+        elif isinstance(initialization, UnitTheoreticalOutputStd):
+            self.epses = nn.ParameterList(
+                epses_composition.make_epses_composition_unit_theoretical_output_std(
+                    epses_specs, 2, device, dtype
+                )
+            )
+        else:
+            raise ValueError(f"{initialization=} is not {Initialization}")
+        pre_linear_image_height = (
+            28 - (kernel_sizes := tuple(ks for ks, _ in epses_specs)) + len(kernel_sizes)
+        )
+        pre_linear_image_width = pre_linear_image_height
+        self.linear = nn.Linear(
+            pre_linear_image_height
+            * pre_linear_image_width
+            * eps.matrix_shape(self.epses[-1])[0],
+            10,
+            bias=True,
+        ).to(dtype)
+        self.linear.weight.data.copy_(
+            torch.randn_like(self.linear.weight) * self.linear.in_features ** -0.5 / 4.0
+        )
+        # TODO where's dropout??!
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError()  # Stopped here, TODO implement this and also see the whiteboard
 
 
 class EPSesPlusLinear(nn.Sequential):
