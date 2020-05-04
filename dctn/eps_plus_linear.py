4bc09aeb6fb7b0839ddf683a051d5ba4a17208e1
Philip B <me@crabman.me>
2020-05-04 22:40:19 +0300

    Refactor EPSesPlusLinear init. Warn: inconsistent with new_runner

--- dctn/eps_plus_linear.py
+++ dctn/eps_plus_linear.py
@@ -13,8 +13,13 @@ from einops.layers.torch import Rearrange
 from einops import rearrange
 
 from . import eps
-from dctn.eps import EPS
+from .eps import EPS
 from . import epses_composition
+from .utils import (
+    ZeroCenteredNormalInitialization,
+    ZeroCenteredUniformInitialization,
+    OneTensorInitialization,
+)
 
 
 @attrs(auto_attribs=True, frozen=True)
@@ -27,7 +32,16 @@ class UnitTheoreticalOutputStd:
     pass
 
 
-Initialization = Union[UnitEmpiricalOutputStd, UnitTheoreticalOutputStd]
+@attrs(auto_attribs=True, frozen=True)
+class ManuallyChosenInitialization:
+    epses: Tuple[OneTensorInitialization]
+    linear_weight: OneTensorInitialization
+    linear_bias: OneTensorInitialization
+
+
+Initialization = Union[
+    UnitEmpiricalOutputStd, UnitTheoreticalOutputStd, ManuallyChosenInitialization
+]
 
 
 class EPSesPlusLinear(nn.Module):
@@ -51,9 +65,18 @@ class EPSesPlusLinear(nn.Module):
             epses = epses_composition.make_epses_composition_unit_theoretical_output_std(
                 epses_specs, 2, device, dtype
             )
+        elif isinstance(initialization, ManuallyChosenInitialization):
+            # TODO change this
+            epses = tuple(
+                epses_composition.make_epses_composition_manually_chosen_inializations(
+                    epses_specs, initialization.epses, 2, device, dtype
+                )
+            )
         else:
             raise ValueError(f"{initialization=} is not {Initialization}")
         self.epses = nn.ParameterList(nn.Parameter(eps_core) for eps_core in epses)
+
+        # initialize the linear layer
         pre_linear_image_height = (
             28 - sum(kernel_sizes := tuple(ks for ks, _ in epses_specs)) + len(kernel_sizes)
         )
@@ -65,9 +88,30 @@ class EPSesPlusLinear(nn.Module):
             10,
             bias=True,
         ).to(dtype)
-        self.linear.weight.data.copy_(
-            torch.randn_like(self.linear.weight) * self.linear.in_features ** -0.5 / 4.0
-        )
+        if isinstance(initialization, ManuallyChosenInitialization):
+            for param, param_initialization in zip(
+                (self.linear.weight, self.linear.bias),
+                (initialization.linear_weight, initialization.linear_bias),
+            ):
+                if isinstance(param_initialization, ZeroCenteredNormalInitialization):
+                    new_data = torch.randn_like(param) * param_initialization.std
+                elif isinstance(param_initialization, ZeroCenteredUniformInitialization):
+                    new_data = (
+                        torch.rand_like(param) * (2 * param_initialization.maximum)
+                        - param_initialization.maximum
+                    )
+                else:
+                    raise ValueError(
+                        f"{initialization=} must be {ManuallyChosenInitialization}"
+                    )
+                param.data.copy_(new_data)
+
+        else:
+            self.linear.weight.data.copy_(
+                torch.randn_like(self.linear.weight) * self.linear.in_features ** -0.5 / 4.0
+            )
+        self.linear.to(device)
+
         self.register_buffer("p", torch.tensor(p, device=device, dtype=dtype))
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
